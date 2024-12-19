6ad826697d5490b9d1108bc85ba6cd1af182d462
lognanil <69664288+lognanil@users.noreply.github.com>
2024-12-19 22:42:54 +0530

    Update dbCredentials.js

--- config/dbCredentials.js
+++ config/dbCredentials.js
@@ -1,18 +1,23 @@
-
-
-
 const dbCredentials = () => {
-  const encodedUser = 'cG9zdGdyZXM='; //postgres
-  const encodedHost = 'bG9jYWxob3N0'; // local
-  const enCodedDatabase = 'VGVzdA=='; // DMS  RE1T//  Test VGVzdA==
-  const encodedPassword = 'c3JpcmFt' //arati password cGFzc3dvcmQ=//sriram   c3JpcmFt
-  const encodedPort = 'NTQzMg=='; // arati / 5432
+  const encodedUser = "test_irqa_user"; //test_irqa_user
+  const encodedHost =
+    "V1GYXe6Nw7X85Rh5RuQONKMofktoE6fg@dpg-cthrfnd6l47c738b049g-a"; // V1GYXe6Nw7X85Rh5RuQONKMofktoE6fg@dpg-cthrfnd6l47c738b049g-a
+  const enCodedDatabase = "test_irqa"; // test_irqa
+  const encodedPassword = "V1GYXe6Nw7X85Rh5RuQONKMofktoE6fg"; // V1GYXe6Nw7X85Rh5RuQONKMofktoE6fg
+  const encodedPort = "5432"; //  5432
   return {
-    encodedUser, encodedHost, enCodedDatabase, encodedPassword, encodedPort
+    encodedUser,
+    encodedHost,
+    enCodedDatabase,
+    encodedPassword,
+    encodedPort,
   };
 };
 
 exports.dbCredentials = dbCredentials;
 
-
-
+// const encodedUser = 'cG9zdGdyZXM='; //postgres
+// const encodedHost = 'bG9jYWxob3N0'; // local
+// const enCodedDatabase = 'VGVzdA=='; // DMS  RE1T//  Test VGVzdA==
+// const encodedPassword = 'c3JpcmFt' //arati password cGFzc3dvcmQ=//sriram   c3JpcmFt
+// const encodedPort = 'NTQzMg=='; // arati / 5432
